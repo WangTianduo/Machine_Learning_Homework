6746ff5d4934b4a7c911f88a5407eba250c9dbb3
Tianduo Wang <wtianduo@gmail.com>
2018-10-14 21:43:55 +0800

    ex3 finish

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -51,13 +51,22 @@ X = [ones(m, 1) X];
 
 
 
+% Set Initial theta
+initial_theta = zeros(n + 1, 1);
 
+% Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
+% Run fmincg to obtain the optimal theta
+% This function will return theta and the cost
 
+% Variable 'X' contains data in dimension (5000 * 400).
+% 5000 = Total no. of training examples, 400 = 400 pixels / training sample (digit image)
+% Total no. Features  = 400
 
-
-
-
+for c = 1:num_labels
+all_theta(c,:) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+end
 
 
 % =========================================================================
